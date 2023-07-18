2ee221ecc8d277cec3c103ff020242ef52319f94
hollisyang <hollis2002@qq.com>
2023-07-18 11:22:12 +0800

    挂载全局echarts对象

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, Vue } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+// 将全局echarts对象挂载到Vue的原型对象上
+// 别的组件中通过 this.$echarts 使用
+Vue.prototype.$echarts = window.echarts
 
 const app = createApp(App)
 
