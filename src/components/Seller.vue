ed1a4c8836cbbc1aafed9e31e97d409b4506f973
hollisyang <hollis2002@qq.com>
2023-07-19 00:34:55 +0800

    柱状图option拆分和分辨率适配

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -19,6 +19,70 @@ let totalPage = 0  // 一共有多少页
 // 初始化 echartsInstance 对象
 function initChart() {
   chartInstance.value = echarts.init(seller_ref.value, 'chalk')
+
+  // 图表初始化配置
+  const initOption = {
+    // 横向柱状图，所以数值轴为x；类目轴为y
+    title: {
+      text: '丨商家销售量统计',
+      left: 20,
+      top: 20
+    },
+    grid: {
+      top: '20%',
+      left: '3%',
+      right: '6%',
+      bottom: '3%',
+      containLabel: true // 使距离包含坐标轴上的label文字
+    },
+    xAxis: {
+      type: 'value'
+    },
+    yAxis: {
+      type: 'category'
+    },
+    tooltip: {
+      trigger: 'axis',
+      // 坐标轴指示器配置项
+      axisPointer: {
+        type: 'line',
+        z: 0,  // 显示层级低于柱体本身
+        lineStyle: {
+          color: '#2d3443',
+          type: 'solid'  // 默认为dashed
+        }
+      }
+    },
+    series: [
+      {
+        type: 'bar',
+        label: {
+          show: true,
+          position: 'right',
+          textStyle: {
+            color: 'white'
+          }
+        },
+        itemStyle: {
+          color: {
+            type: 'linear',  // 线性渐变
+            // 渐变方向 →
+            x: 0,
+            y: 0,
+            x2: 1,
+            y2: 0,
+            colorStops: [{
+              offset: 0, color: '#5052ee' // 0% 处的颜色
+            }, {
+              offset: 1, color: '#ab6ee5' // 100% 处的颜色
+            }],
+          }
+        }
+      }
+    ]
+  }
+  chartInstance.value.setOption(initOption)
+
   // 监听鼠标位置(如果鼠标在图表内就关闭定时器)
   chartInstance.value.on('mouseover', () => {
     clearInterval(timeId.value)
@@ -66,86 +130,68 @@ function updateChart() {
   // 获取其中的name和value
   const sellerNames = showData.map((item) => item.name)
   const sellerValues = showData.map((item) => item.value)
-  const option = {
-    // 横向柱状图，所以数值轴为x；类目轴为y
-    title: {
-      text: '丨商家销售量统计',
-      textStyle: {
-        fontSize: 66
-      },
-      left: 20,
-      top: 20
-    },
-    grid: {
-      top: '20%',
-      left: '3%',
-      right: '6%',
-      bottom: '3%',
-      containLabel: true // 使距离包含坐标轴上的label文字
-    },
-    xAxis: {
-      type: 'value'
-    },
+
+  // 图表数据配置
+  const dataOption = {
     yAxis: {
-      type: 'category',
       data: sellerNames
     },
+    series: [{
+      data: sellerValues,
+    }]
+  }
+  chartInstance.value.setOption(dataOption)
+}
+
+// 当window大小变化时调用，完成屏幕适配
+function screenAdapter() {
+  // 容器的宽度 `seller_ref.value.offsetWidth`
+  const titleFontSize = seller_ref.value.offsetWidth / 100 * 3.6
+  
+  // 图表分辨率相关参数配置
+  const adapterOption = {
+    // 横向柱状图，所以数值轴为x；类目轴为y
+    title: {
+      textStyle: {
+        fontSize: titleFontSize
+      }
+    },
     tooltip: {
-      trigger: 'axis',
       // 坐标轴指示器配置项
       axisPointer: {
-        type: 'line',
-        z: 0,  // 显示层级低于柱体本身
         lineStyle: {
-          width: 66,
-          color: '#2d3443',
-          type: 'solid'  // 默认为dashed
+          width: titleFontSize
         }
       }
     },
     series: [
       {
-        type: 'bar',
-        data: sellerValues,
-        barWidth: 66,
-        label: {
-          show: true,
-          position: 'right',
-          textStyle: {
-            color: 'white'
-          }
-        },
+        barWidth: titleFontSize,
         itemStyle: {
           // 柱体的圆角
-          borderRadius: [0, 33, 33, 0],
-          color: {
-            type: 'linear',  // 线性渐变
-            // 渐变方向 →
-            x: 0,
-            y: 0,
-            x2: 1,
-            y2: 0,
-            colorStops: [{
-              offset: 0, color: '#5052ee' // 0% 处的颜色
-            }, {
-              offset: 1, color: '#ab6ee5' // 100% 处的颜色
-            }],
-          }
+          borderRadius: [0, titleFontSize / 2, titleFontSize / 2, 0]
         }
       }
     ]
   }
-  chartInstance.value.setOption(option)
+  chartInstance.value.setOption(adapterOption)
+  
+  // 屏幕大小改变后，需要调用图表实例对象 `chartInstance` 的 `resize` => 才能产生新图表
+  chartInstance.value.resize()
 }
 
 onMounted(() => {
   initChart()
-})
-onMounted(() => {
   getData()
+  // 监听window大小变化以进行分辨率适配
+  window.addEventListener('resize', screenAdapter)
+  // 界面加载完成后主动进行分辨率适配
+  screenAdapter()
 })
 onUnmounted(() => {
   clearInterval(timeId.value)
+  // 组件销毁时取消事件监听
+  window.removeEventListener('resize', screenAdapter)
 })
 </script>
 
