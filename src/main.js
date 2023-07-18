3ed37b127792a8fb1496f5cd0f7b3c2c1e0efcce
hollisyang <hollis2002@qq.com>
2023-07-18 11:24:29 +0800

    配置axios

--- src/main.js
+++ src/main.js
@@ -6,9 +6,19 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// 配置axios
+import axios from 'axios'
+// 请求基准路径配置
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api'
+// 将axios挂载到Vue的原型对象上
+// 别的组件中通过 this.$http 使用
+Vue.prototype.$http = axios
+
+
 // 将全局echarts对象挂载到Vue的原型对象上
 // 别的组件中通过 this.$echarts 使用
 Vue.prototype.$echarts = window.echarts
+
 
 const app = createApp(App)
 
