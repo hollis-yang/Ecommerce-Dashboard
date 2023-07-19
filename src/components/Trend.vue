94c3d6c164385b5a8a22157c3f393be72bdeb2c9
hollisyang <hollis2002@qq.com>
2023-07-19 13:48:52 +0800

    折线图ui调整

--- src/components/Trend.vue
+++ src/components/Trend.vue
@@ -34,6 +34,11 @@ function initChart() {
     },
     yAxis: {
       type: 'value'
+    },
+    legend: {
+      left: 20,
+      top: '15%',
+      icon: 'circle'
     }
   }
   chartInstance.setOption(initOption)
@@ -49,18 +54,53 @@ async function getData() {
 }
 
 function updateChart() {
+  // 一个折线图的颜色由半透明 -> 全透明
+  // 半透明的颜色值
+  const colorArr1 = [
+    'rgba(11, 168, 44, 0.5)',
+    'rgba(44, 110, 255, 0.5)',
+    'rgba(22, 242, 217, 0.5)',
+    'rgba(254, 33, 30, 0.5)',
+    'rgba(250, 105, 0, 0.5)'
+  ]
+  // 全透明的颜色值
+  const colorArr2 = [
+    'rgba(11, 168, 44, 0)',
+    'rgba(44, 110, 255, 0)',
+    'rgba(22, 242, 217, 0)',
+    'rgba(254, 33, 30, 0)',
+    'rgba(250, 105, 0, 0)'
+  ]
+
   // 数据处理
   const timeArr = allData.value.common.month  // x轴类目轴数据, 所有x轴的数据
   // y轴数据
   const valueArr = allData.value.map.data  // 地区销量趋势
   console.log(valueArr)
+
   // 准备 series: [{type: ..., data: ...}, {}, {}, ...]
-  const seriesArr = valueArr.map(item => {
+  const seriesArr = valueArr.map((item, index) => {
     return {
       name: item.name,  // 用于图例匹配
       type: 'line',
       data: Array.from(item.data),
-      stack: 'map'  // stack 设置为相同的字符串的数据会显示在同一堆叠图上
+      stack: 'map',  // stack 设置为相同的字符串的数据会显示在同一堆叠图上
+      areaStyle: {
+        color: {
+          // areaStyle颜色渐变
+          type: 'linear',  // 线性渐变
+          // 渐变方向 ↓
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [{
+            offset: 0, color: colorArr1[index] // 0% 处的颜色
+          }, {
+            offset: 1, color: colorArr2[index] // 100% 处的颜色
+          }],
+        }
+      }
     }
   })
   // 准备图例
